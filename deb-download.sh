e3f3069e7b7d85efa00daaace5549b65511ca4a5
N0rbert <nrbrtx@gmail.com>
2023-04-30 20:02:33 +0300

    stretch is EOL, add mantic

--- deb-download.sh
+++ deb-download.sh
@@ -36,15 +36,15 @@ third_party_repo_command="true"
 get_source_command="true"
 
 # distros and their versions
-supported_ubuntu_releases="trusty|xenial|bionic|focal|jammy|kinetic|lunar|devel";
+supported_ubuntu_releases="trusty|xenial|bionic|focal|jammy|kinetic|lunar|mantic|devel";
 eol_ubuntu_releases="precise|quantal|raring|saucy|utopic|vivid|wily|yakkety|zesty|artful|cosmic|disco|eoan|groovy|hirsute|impish";
 ubuntu_release_is_eol=0;
 
-supported_debian_releases="oldoldstable|stretch|oldstable|buster|stable|bullseye";
+supported_debian_releases="oldstable|buster|stable|bullseye";
 debian_releases_newsecurity="stable|bullseye";
 testing_debian_releases="bookworm|testing";
 rolling_debian_releases="sid|unstable|experimental";
-eol_debian_releases="squeeze|wheezy|jessie";
+eol_debian_releases="squeeze|wheezy|jessie|stretch";
 debian_release_is_eol=0;
 
 supported_mint_releases="19$|19.1|19.2|19.3|20$|20.1|20.2|20.3|21$|21.1";
